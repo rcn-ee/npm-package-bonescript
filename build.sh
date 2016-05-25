d687cc60751f3cf3c2f639b8c7124af928b8c2e1
Robert Nelson <robertcnelson@gmail.com>
2016-05-25 17:06:24 -0500

    initial build
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+
+export NODE_PATH=/usr/local/lib/node_modules
+
+npm_options="--unsafe-perm=true --progress=false --loglevel=error --prefix /usr/local"
+
+npm_git_install () {
+	if [ -d /usr/local/lib/node_modules/${npm_project}/ ] ; then
+		rm -rf /usr/local/lib/node_modules/${npm_project}/ || true
+	fi
+
+	wrepo="bonescript"
+	if [ -d /tmp/${wrepo}/ ] ; then
+		rm -rf /tmp/${wrepo}/ || true
+	fi
+
+	git clone -b 0.5.x https://github.com/jadonk/${wrepo} /tmp/${wrepo}
+	if [ -d /tmp/${wrepo}/ ] ; then
+		cd /tmp/${wrepo}/
+		package_version=$(cat package.json | grep version | awk -F '"' '{print $4}' || true)
+		git_version=$(git rev-parse --short HEAD)
+		TERM=dumb ${node_bin} ${npm_bin} install -g ${npm_options}
+		cd -
+		rm -rf /tmp/${wrepo}/
+	fi
+
+	cd /usr/local/lib/node_modules/
+	if [ -f ${npm_project}-${package_version}-${git_version}-${node_version}.tar.xz ] ; then
+		rm -rf ${npm_project}-${package_version}-${git_version}-${node_version}.tar.xz || true
+	fi
+	tar -cJf ${npm_project}-${package_version}-${git_version}-${node_version}.tar.xz ${npm_project}/
+	cd -
+
+	if [ ! -f ./deploy/${npm_project}-${package_version}-${git_version}-${node_version}.tar.xz ] ; then
+		cp -v ${npm_project}/${npm_project}-${package_version}-${git_version}-${node_version}.tar.xz ./deploy/
+		echo "New Build: ${npm_project}-${package_version}-${git_version}-${node_version}.tar.xz"
+	fi
+}
+
+npm_install () {
+	node_bin="/usr/bin/nodejs"
+	npm_bin="/usr/bin/npm"
+
+	unset node_version
+	node_version=$(/usr/bin/nodejs --version || true)
+
+	echo "npm: [`${node_bin} ${npm_bin} --version`]"
+	echo "node: [`${node_bin} --version`]"
+
+	npm_project="bonescript"
+	npm_git_install
+}
+
+npm_install
